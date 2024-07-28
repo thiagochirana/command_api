8944d5818e85200c09157593946b469f86c6d74a
Chirana <engsw.thiago@gmail.com>
2024-07-28 15:29:10 -0300

    add site

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,6 +92,8 @@ Rails.application.configure do
   #   "example.com",     # Allow requests from example.com
   #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
   # ]
+
+  config.hosts << "/.*\.devcurumin\.com\.br/"
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 end
